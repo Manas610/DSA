5bfb91fe0f8debda446d7622d1301d8d8423cf11
Manassingh610 <72378020+Manas610@users.noreply.github.com>
2023-02-16 22:57:19 +0530

    quickSort

---
+++ Phase 2/quickSort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int partition(int *arr , int s , int e){
+    int pivot = arr[s];
+    int count = 0;
+    for(int i=s+1;i<=e;i++){
+        if(arr[i] <= pivot){
+            count++;
+        }
+    }
+    int pivotIndex = s + count;
+    swap(arr[pivotIndex] , arr[s]);
+
+    int i = s, j = e;
+    while(i<pivotIndex && j>pivotIndex){
+        while(arr[i]<=pivot){
+            i++;
+        }
+        while(arr[j]>pivot){
+            j--;
+        }
+        if(i<pivotIndex && j>pivotIndex){
+            swap(arr[i++],arr[j--]);
+        }
+    }
+}
+
+void quickSort(int *arr , int s , int e){
+    if(s>=e){
+        return;
+    }
+
+    int p = partition(arr, s, e);
+
+    quickSort(arr , s , p-1);
+
+    quickSort(arr , p+1 , e);
+}
+
+int main(){
+    int arr[8] ={6, 7 , -6, -2, -4, -6, 2, -6 };
+    int n = 8;
+    quickSort(arr , 0 , n-1);
+
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+}
